4aec68dbdce483ac11f9202e103afa45bd168c47
Darcy Luo <59079083+DarcyLuo@users.noreply.github.com>
2026-01-17 22:22:41 -0800

    no missing data

--- analysis.py
+++ analysis.py
@@ -1,3 +1,9 @@
 import pandas as pd
 df=pd.read_csv("data/abcnews-date-text.csv")
 print(df.head())
+
+#exploratory analysis
+#count headline length
+df['headline_length']=df['headline_text'].str.len()
+print(df.head())
+print(df['headline_length'].info())
