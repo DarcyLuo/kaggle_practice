64e6a6b2ee54be905ddccc1bf324f0b831dfc9be
Darcy Luo <59079083+DarcyLuo@users.noreply.github.com>
2026-01-20 21:44:40 -0800

    continue descriptive analysis

--- analysis.py
+++ analysis.py
@@ -7,3 +7,15 @@ print(df.head())
 df['headline_length']=df['headline_text'].str.len()
 print(df.head())
 print(df['headline_length'].info())
+#take-away: no missing data
+
+#count headline words
+df['headline_word_count'] = df['headline_text'].str.split().str.len()
+print(df.head())
+print(df['headline_word_count'].info())
+
+print(df['headline_word_count'].min())
+print(df['headline_word_count'].max())
+
+#how many headline word count is 1 and what are they?
+
